b750e155320e3f860194d522a45d21485d4f5637
kimminkyeu <minkyeki@student.42seoul.kr>
2023-06-21 15:51:29 +0900

    d

--- libasm/ft_strlen.s
+++ libasm/ft_strlen.s
@@ -1,32 +1,26 @@
-
 BITS 64 ; for vim syntastic
-; Parameters to functions are passed in the registers
-; rdi, rsi, rdx, rcx, r8, r9, and further values are passed on the stack in reverse order.
-; https://www.youtube.com/watch?v=kjOcgG0FpNQ
 
 section .text
-	global ft_strlen ;; if MacOS, needs underbar _ft_strlen.
-
-; [ How to use stack in asm ]
-; https://www.youtube.com/watch?v=vcfQVwtoyHY
-; https://www.youtube.com/watch?v=5eWiz3soaEM
-
-ft_strlen:
+	global _ft_strlen
+_ft_strlen:
 	push rbp
-	mov rbp, rsp
 	mov qword [rbp - 8], rdi
+	; ** 내가 체크해야 할 가설: 위 연산을 하면 rsp가 바뀌는지 반드시 체크...
+	; 왜냐면, 이 행위가 stack에 ft_strlen의 local variable frame을
+	; 넣는 행위잖아.... 그런데... 이 행위가 rsp를 바꾸지 않으면... 잘못된거 아냐?
+	; rsp는 항상 stack의 끝을 가리켜야 하니까...
 	mov qword [rbp - 16], 0
  .PROC_CHECK_NULL:
 	mov rax, qword [rbp - 8]
 	mov rcx, qword [rbp - 16]
 	cmp byte [rax + rcx], 0
-	je  ft_strlen.PROC_RETURN_LEN
+	je  _ft_strlen.PROC_RETURN_LEN
  .PROC_INCREMENT_COUNT:  
 	mov rax, qword [rbp - 16]
 	add rax, 1
 	mov qword [rbp - 16], rax
-	jmp ft_strlen.PROC_CHECK_NULL
+	jmp _ft_strlen.PROC_CHECK_NULL
  .PROC_RETURN_LEN:
 	mov rax, qword [rbp - 16]
-	pop rbp
+	pop rbp 
 	ret
